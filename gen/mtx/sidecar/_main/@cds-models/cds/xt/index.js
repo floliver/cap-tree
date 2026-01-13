c65c5845c8f31c17b0e7a7628074756cc79d4c8c
Florian Oliver <Florian.Oliver@softproviding.com>
2026-01-13 10:12:18 +0100

    lamas rule the world

--- gen/mtx/sidecar/_main/@cds-models/cds/xt/index.js
+++
@@ -1,8 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-import { createEntityProxy } from './../../_/index.js'
-// Extensions
-export const Extension = createEntityProxy(['cds.xt', 'Extensions'], { target: { is_singular: true } })
-export const Extensions = createEntityProxy(['cds.xt', 'Extensions'], { target: { is_singular: false }})
-// events
-// actions
-// enums
--- gen/srv/@cds-models/cds/xt/MTXServices/index.js
+++
@@ -1,8 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-import { createEntityProxy } from './../../../_/index.js'
-// service
-const MTXServices = { name: 'cds.xt.MTXServices' }
-export default MTXServices
-// events
-// actions
-// enums
